7c4960700151ad8e97bce8804127891662de1dbc
Jon Long <jon@linesandwaves.com>
2014-08-25 09:19:32 -0400

    use official mackup dist

--- mackup/install.sh
+++ mackup/install.sh
@@ -4,12 +4,10 @@
 #
 # Install mackup
 
-mackup_dir="$PROJECTS/mackup"
-
 # Check for mackup
 if [ ! -d $mackup_dir ]; then
   echo "Installing mackup for you."
-  git clone git@github.com:jonlong/mackup.git $mackup_dir
+  brew install mackup
 fi
 
 exit 0
